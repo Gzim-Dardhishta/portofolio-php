bddff659b583652274688cbb4014844c6ff813eb
Gzim-DArdhishta <gzimdardhishta0gmail.com>
2022-11-10 01:20:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Gzim Portofolio</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <h1>XIMI</h1>
+        </div>
+        <nav>
+            <ul class="nav-links">
+                <li class="nav-link"><a href="#home">#Home</a></li>
+                <li class="nav-link"><a href="#about">#About</a></li>
+                <li class="nav-link"><a href="#">#Skills</a></li>
+                <li class="nav-link"><a href="#">#Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div id="home">
+            <div class="introduce">
+                <p>Hi, I am Ximi!</p>
+                <h3>I'm Software Developer</h3>
+            </div>
+
+            <div class="image">
+                <img src="./images/bitmoji.jpg" alt="" width="300" height="300">
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
